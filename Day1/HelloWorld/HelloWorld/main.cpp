e866665c5ffb84c1f822f315ef855d43f735d4c5
Branden-Frieden <bcfrieden@gmail.com>
2022-08-22 11:49:38 -0600

    committing my hello world project to git

---
+++ Day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Branden Frieden on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
