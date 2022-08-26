43eb41f7875dc1b68b702e4a8011b04d5c561e2f
Branden-Frieden <bcfrieden@gmail.com>
2022-08-26 07:48:37 -0600

    commit for update to Day4 date formats project

--- Day4/DateFormats/DateFormats/main.cpp
+++ Day4/DateFormats/DateFormats/main.cpp
@@ -3,23 +3,46 @@
 //  DateFormats
 //
 //  Created by Mack Tawa, Branden Frieden, Ricardo Sanchezyepez 8/25/22.
-//
+// Asks user for date using mm/dd/yyyy and forms a word version out of it
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 int main() {
     string dateInput;
     string monthWord;
+    string fillerZero;
  
     
     
 // Asking for input
     cout << "Enter a date in mm/dd/yyyy format. \n";
     cin >> dateInput;
+    
+    if (dateInput.find("/")!=2){
+        dateInput.insert(0, "0");
+        
+    }
+    if (dateInput.length()==9){
+        dateInput.insert(3, "0");
+    }
 // creating 2 letter string from their input
     monthWord = dateInput[0];
     monthWord += dateInput[1];
+
+// if month is greater than 12 it asks for a valid date
+    while (stoi(monthWord) > 12) {
+        (cout << "Invalid date. Re-enter valid date.");
+        cin >> dateInput;
+        monthWord = dateInput[0];
+        monthWord += dateInput[1];
+    }
+
+
 // allocating a month to the dates
+
+
     if (monthWord == "01") {
         cout << "January";
     }
@@ -56,7 +79,7 @@ int main() {
     else (cout << "December");
 // outputting desired formatted date
     cout << " " << dateInput[3] << dateInput[4] << ", " << dateInput[6] << dateInput[7] << dateInput[8] << dateInput[9] << endl;
-    
-    
+
+
     return 0;
 }
