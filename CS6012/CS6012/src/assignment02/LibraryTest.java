baad34cedd1a772882784dc84ef4fd374a647fc1
Branden-Frieden <bcfrieden@gmail.com>
2022-11-10 11:17:29 -0700

    update to assignment02 tests and upload for Lab 04

--- CS6012/CS6012/src/assignment02/LibraryTest.java
+++ CS6012/CS6012/src/assignment02/LibraryTest.java
@@ -51,7 +51,28 @@ class LibraryTest {
         var lib = new Library();
         lib.addAll("Mushroom_Publishing.txt");
 
-        // FILL IN MORE TESTS HERE!
+
+        var res = lib.checkout( 9781843190394L, "Branden", 11, 1, 2021);
+        assertTrue(res);
+
+        var res2 = lib.checkout( 9781843193319L, "Branden", 11, 1, 2021);
+        assertTrue(res2);
+
+
+        assertEquals("Branden", lib.lookup(9781843190394L));
+        assertEquals("Branden", lib.lookup(9781843193319L));
+
+        ArrayList<LibraryBook> checkedOutBooks = lib.lookup("Branden");
+
+        assertEquals( new LibraryBook(9781843190394L, "Kate Clarke", "The Royal United Hospital") ,checkedOutBooks.get(0));
+        assertEquals( new LibraryBook(9781843193319L, "Alan Burt Akers", "Transit to Scorpio") ,checkedOutBooks.get(1));
+
+        assertEquals(new GregorianCalendar(2021, 11, 1), checkedOutBooks.get( 0 ).getDueDate());
+        lib.checkin("Branden");
+
+
+        assertNull(lib.lookup(9781843190394L));
+        assertNull(lib.lookup(9781843193319L));
     }
 
 }
