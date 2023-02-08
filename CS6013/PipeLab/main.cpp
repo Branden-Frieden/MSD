ffb822c0bf03378d2e363a59ede7580f7afaed19
Branden-Frieden <bcfrieden@gmail.com>
2023-02-08 15:49:00 -0700

    finished pipe lab

--- CS6013/PipeLab/main.cpp
+++ CS6013/PipeLab/main.cpp
@@ -1,49 +1,37 @@
 #include <iostream>
-#include <sstream>
-#include <cstdio>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <cstdlib>
-#include <csignal>
-#include <filesystem>
-#include <sys/file.h>
 #include <array>
 
 int main(int argc, char *argv[]){
-
 
     int fd[2];
     if(pipe(fd) < 0){
         std::cout << "error in opening pipe";
         return -1;
     }
-
     int rc = fork();
 
     if(rc < 0){
         std::cout << "error\n";
     } else if(rc == 0){
 
-        std::cout << "child\n";
         close(fd[1]);
-        char buff[64];
-        read(fd[0], buff, 64);
-
+        int size = 0;
+        read(fd[0], &size, sizeof(size));
+        char buff[size];
+        read(fd[0], buff, size);
         std::cout << buff;
-        fflush(stdout);
-
         close(fd[0]);
 
     } else{
 
-        std::cout << "parent\n";
-        fflush(stdout);
         close(fd[0]);
+        int size = strlen(argv[1]);
+        write(fd[1], &size , sizeof(size));
         write(fd[1], argv[1], strlen(argv[1]));
-
         wait(NULL);
         close(fd[1]);
-
 
     }
 
