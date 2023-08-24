e80d0efa3de45cac4394f94667651cac14e322d8
Branden-Frieden <bcfrieden@gmail.com>
2023-08-24 10:34:45 -0600

    lab1

---
+++ CS6018/lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.lab1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener(this)
+        findViewById<Button>(R.id.button2).setOnClickListener(this)
+        findViewById<Button>(R.id.button3).setOnClickListener(this)
+        findViewById<Button>(R.id.button4).setOnClickListener(this)
+        findViewById<Button>(R.id.button5).setOnClickListener(this)
+
+
+
+    }
+
+    override fun onClick(v: View?) {
+        val b = v as Button
+        val string = b.text.toString()
+        val intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("string", string)
+
+        startActivity(intent)
+    }
+}
---
+++ CS6018/lab1/app/src/main/java/com/example/lab1/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val string = intent.getStringExtra("string")
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = string
+
+    }
+}
